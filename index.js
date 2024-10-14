1c3660cff2680e517efa4b2be34a684f14496ad6
Denis Chuvakov <ds.chuvakov@gmail.com>
2024-10-15 02:34:51 +0300

    3.4: Phonebook backend step 4

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+// delete person by id
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
