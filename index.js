95f956830efd2496b619181b9871d73c3232988b
Denis Chuvakov <ds.chuvakov@gmail.com>
2024-11-17 21:15:12 +0200

    Exercises 3.15.-3.18.

--- index.js
+++ index.js
@@ -8,38 +8,49 @@ const Person = require("./models/person");
 app.use(cors());
 mongoose.set("strictQuery", false);
 morgan.token("body", (request) => JSON.stringify(request.body));
+
+app.use(express.static("dist"));
 app.use(express.json());
+//app.use(requestLogger)
 app.use(morgan("tiny"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-app.use(express.static("dist"));
+
+console.log("Database URI: ", process.env.MONGODB_URI);
 
 // display the time  and how many entries are in the phonebook
-app.get("/info", (req, res) => {
-  const entries = persons.length;
+app.get("/info", (req, res, next) => {
   const timeNow = new Date();
-  res.send(`Phonebook has info for ${entries} people <br/> ${timeNow}`);
+  Person.find({})
+    .then((persons) => {
+      const entries = persons.length;
+      res.send(`Phonebook has info for ${entries} people <br/> ${timeNow}`);
+    })
+    .catch((error) => next(error));
 });
 
 // get all persons
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then((persons) => {
-    res.json(persons);
-  });
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
 // get person by id
-app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    console.log(`Couldn't fetch the person with id ${id}.`);
-    res.status(404).end();
-  }
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        console.log(`Couldn't fetch the person with id ${id}.`);
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 // create a new person
@@ -49,28 +60,62 @@ app.post("/api/persons", (req, res, next) => {
   if (!name || !number) {
     return res.status(400).json({ error: "Name or number is missing" });
   }
+
   Person.findOne({ name })
     .then((existingPerson) => {
       if (existingPerson) {
-        return res.status(400).json({ error: "Name must be unique" });
+        existingPerson.number = number;
+        return existingPerson.save();
+      } else {
+        const person = new Person({ name, number });
+        return person.save();
       }
-
-      const person = new Person({ name, number });
-
-      return person.save();
     })
     .then((savedPerson) => {
       res.status(201).json(savedPerson);
     })
-    .catch((error) => next(error)); 
+    .catch((error) => next(error));
 });
 
 // delete person by id
-app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
+
+// update person
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
